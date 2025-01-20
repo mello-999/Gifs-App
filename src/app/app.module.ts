15084ac2e99e4806280b0ce2c953bb73f8748ee9
Mauricio donoso <donosomauricio721@gmail.com>
2025-01-20 16:57:54 -0300

    Importaciones-Listas-en App-Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SahredModule } from './sahred/sahred.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    GifsModule,
+    SahredModule
+    // AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
