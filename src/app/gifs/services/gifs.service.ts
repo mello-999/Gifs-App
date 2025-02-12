4747faf2bcd38fc48bbcdc38bb3982da9bd6de72
Mauricio donoso <donosomauricio721@gmail.com>
2025-02-12 02:47:23 -0300

    Sección-7-Terminado

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,10 @@ export class GifsService {
   private apikey:       string = 'UOjIkqCyQXPZkEHTUgiVo6LuGwpfEvo7';
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+   }
 
 
   get tagsHistory() {
@@ -28,12 +31,22 @@ export class GifsService {
 }
 
         this._tagsHistory.unshift( tag );
-
         this._tagsHistory = this._tagsHistory.splice(0,10);
-
-
+        this.saveLocalStorage();
       }
 
+     private saveLocalStorage():void{
+      localStorage.setItem( 'history', JSON.stringify ( this._tagsHistory) );
+     }
+      private loadLocalStorage():void {
+        if( !localStorage.getItem('history') ) return;
+
+        this._tagsHistory = JSON.parse (localStorage.getItem('history')!  );
+
+        if (this._tagsHistory.length === 0) return;
+        this.searchTag(this._tagsHistory[0]);
+
+      }
 
 
     searchTag( tag: string ):void {
