bd4d9b8aacca72c20c221a3e4a0c05284d36ba28
Mauricio donoso <donosomauricio721@gmail.com>
2025-02-13 20:53:12 -0300

    Cap.107-Listo

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 
 
@@ -17,6 +18,7 @@ import { CardListComponent } from './components/card-list/card-list.component';
     HomePageComponent,
     SearchBoxComponent,
     CardListComponent,
+    CardComponent,
 
   ],
   imports: [
